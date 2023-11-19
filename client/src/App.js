ec4c7c4ec48ed90bf36ec8322d09b03a18421fc8
Tess Gompper <tgomp9@gmail.com>
2023-11-19 15:57:56 -0500

    initial footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
  // We import all the components we need in our app
-import Navbar from "./components/navbar";
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
 import About from "./components/about";
 
  const App = () => {
@@ -12,6 +13,7 @@ import About from "./components/about";
      <Routes>
        <Route path="/about" element={<About />} />
      </Routes>
+     <Footer />
    </div>
  );
 };
