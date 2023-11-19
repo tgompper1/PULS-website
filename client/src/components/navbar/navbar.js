c8fe9c9a18889c6e31a4a81ac97b909b236eb4e4
Tess Gompper <tgomp9@gmail.com>
2023-11-19 15:57:43 -0500

    clean up navbar

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 // We import bootstrap to make our application look better.
 import './navbar.css'
+import '../twelve_column_grid.css'
 
 // import "bootstrap/dist/css/bootstrap.css";
  // We import NavLink to utilize the react router.
@@ -8,10 +9,11 @@ import { NavLink } from "react-router-dom";
  // Here, we display our Navbar
 export default function Navbar() {
  return (
-   <div>
+   <div class="col-12 col-s-12">
      <nav className="navbar">
        <NavLink className="navbar-logo" to="/">
-            <img style={{"width" : 40 + '%'}} src="images\logo_color.png"></img>
+            {/* <img style={{"width" : 40 + '%'}} src="images\logo_color.png"></img> */}
+            <img src="images\logo_color.png" width={120} height={80}></img>
        </NavLink>
         <div className="navbar-container container" id="navbarSupportedContent">
             <ul className='nav-menu'>
