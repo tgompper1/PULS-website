1db8e646d77d9aad5ebf44a1be39e33fefd0e7fb
Tess Gompper <tgomp9@gmail.com>
2023-11-28 12:35:51 -0500

    clena up navbar code

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,18 +1,13 @@
 import React from "react";
-// We import bootstrap to make our application look better.
 import './navbar.css'
 import '../twelve_column_grid.css'
 
-// import "bootstrap/dist/css/bootstrap.css";
- // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
- // Here, we display our Navbar
 export default function Navbar() {
  return (
    <div class="col-12 col-s-12">
      <nav className="navbar">
        <NavLink className="navbar-logo" to="/">
-            {/* <img style={{"width" : 40 + '%'}} src="images\logo_color.png"></img> */}
             <img src="images\logo\logo_color.png" width={120} height={80}></img>
        </NavLink>
         <div className="navbar-container container" id="navbarSupportedContent">
