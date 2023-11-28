765f403b6c8ff357a4881a627c19cc2bb2560f26
Tess Gompper <tgomp9@gmail.com>
2023-11-28 12:26:43 -0500

    cleanup carousel code

--- client/src/components/imageCarousel/carouselItem.js
+++ client/src/components/imageCarousel/carouselItem.js
@@ -3,9 +3,7 @@ import React from "react";
 export default function CarouselItem({ imgUrl, imgTitle }) {
   return (
     <div className="carousel-card">
-      {/* <div className="img-div"> */}
-        <img src={imgUrl} alt={imgTitle}></img>
-        {/* </div> */}
+      <img src={imgUrl} alt={imgTitle}></img>
     </div>
   );
 }
