285a939151eac04f1bc0307204ad16cd160dd182
Tess Gompper <tgomp9@gmail.com>
2023-12-02 15:01:43 -0500

    tess's updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
  // We import all the components we need in our app
@@ -9,6 +9,10 @@ import Home from "./components/Home";
 import AdminBlogList from "./components/blog/BlogListAdmin";
 import CreateBlogPost from "./components/blog/CreateBlogPost";
 import BlogList from "./components/blog/BlogList";
+import EventsCalendar from "./components/calendar/Calendar";
+import AdminEventsCalendar from "./components/calendar/adminCalendar";
+import EditPostAdmin from "./components/blog/BlogListAdmin";
+import PostDetails from "./components/blog/PostDetails";
 
 import './styles/general.css';
 
@@ -16,19 +20,21 @@ const App = () => {
  return (
    <div>
      <Navbar />
-     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/admincalendar" element={<AdminEventsCalendar />} />
-        <Route path="/calendar" element={<EventsCalendar />} />
-        <Route path="/blog" element={<BlogList />} />
-        <Route path="/blog-admin" element={<AdminBlogList />} />
-        <Route path="/create-post" element={<CreateBlogPost />} />
-        <Route path="/edit-post/:id" element={<EditPostAdmin />} />
-        <Route path="/post/:id" element={<PostDetails />} />
-        <Route path="/create-post" element={<CreateBlogPost />} />
-        <Route path="/edit-post/:id" element={<EditPostAdmin />} />
-     </Routes>
+     <div className='page-content'>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/admincalendar" element={<AdminEventsCalendar />} />
+            <Route path="/calendar" element={<EventsCalendar />} />
+            <Route path="/blog" element={<BlogList />} />
+            <Route path="/blog-admin" element={<AdminBlogList />} />
+            <Route path="/create-post" element={<CreateBlogPost />} />
+            <Route path="/edit-post/:id" element={<EditPostAdmin />} />
+            <Route path="/post/:id" element={<PostDetails />} />
+            <Route path="/create-post" element={<CreateBlogPost />} />
+            <Route path="/edit-post/:id" element={<EditPostAdmin />} />
+        </Routes>
+      </div>
      <Footer />
    </div>
  );
